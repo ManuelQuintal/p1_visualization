a82d481a69ffb8cada0d0c3167790c0416199cc8
ManuelQuintal <manuel199823@hotmail.com>
2021-08-25 15:48:26 -0400

    start of p1

---
+++ p1.R
@@ -0,0 +1,7 @@
+# - [ ] Recreate the two graphics in this repo using `gapminder` dataset from `library(gapminder)` (get them to match as closely as you can).
+#     - [ ] Use `library(tidyverse)` to load ggplot2 and dplyr and the `theme_bw()` to duplicate the first plot.
+#     - [ ] Use `scale_y_continuous(trans = "sqrt")` to get the correct scale on the y-axis.
+#     - [ ] Build weighted average data set using `weighted.mean()` and GDP with `summarise()` and `group_by()` that will be the black continent average line on the second plot.
+#     - [ ] Use `theme_bw()` to duplicate the second plot. You will need to use the new data to make the black lines and dots showing the continent average.
+#     - [ ] Use `ggsave()` and save each plot as a .png with a width of 15 inches.
+
